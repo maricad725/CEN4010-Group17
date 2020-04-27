1838cac097694e0685c12e05d92b9e9ae68cb8af
Alex D. Lopez <[maricad725@gmail.com]>
2020-04-27 18:28:07 -0400

    updated server and added pkg-lock orig file

--- server.js
+++ server.js
@@ -13,27 +13,33 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect("mongodb+srv://mllanes19:mllanes19@cluster0-tehc9.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+mongoose.connect('mongodb+srv://mllanes19:mllanes19@cluster0-tehc9.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const bookRoutes = require('./routes/api/books');
-const cartRoutes = require('./routes/api/carts');
-const userRoutes = require('./routes/api/users');
-const orderRoutes = require('./routes/api/orders');
+const bookRoutes = require("./routes/api/books");
+const commentRoutes = require("./routes/api/comment");
+const cartRoutes = require("./routes/api/carts");
+const userRoutes = require("./routes/api/users");
+const orderRoutes = require("./routes/api/orders");
+const wishRoutes = require("./routes/api/wishList");
 
-app.use('/books', bookRoutes);
-app.use('/users', userRoutes); 
-app.use('/carts', cartRoutes);
-app.use('/orders', orderRoutes);
+app.use("/books", bookRoutes);
+app.use("/comment", commentRoutes);
+app.use("/users", userRoutes);
+app.use("/carts", cartRoutes);
+app.use("/orders", orderRoutes);
+app.use("/wishList", wishRoutes);
+
 app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'Not Found'
-    })
-})
+  res.status(404).json({
+    message: "Not Found",
+  });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
